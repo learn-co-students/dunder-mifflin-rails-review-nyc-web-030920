8c00e2eafb1030289163611ea6d405c924d00e8c
Lix311 <alex.sterrantino@gmail.com>
2020-04-08 17:51:27 -0400

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,29 @@
 class DogsController < ApplicationController
+    def index 
+        @dogs = Dog.all 
+    end 
+
+    def new 
+        @dog = Dog.new 
+    end 
+
+
+    def create 
+        @dog = Dog.create(dog_params)
+        redirect_to dog_path(@dog)
+    end 
+
+    def show 
+        @dog = Dog.find(params[:id])
+    end 
+
+    def edit 
+        @dog = Dog.find(params[:id])
+    end 
+
+    def update 
+        @dog = Dog.find(params[:id])
+        @dog.update(dog_params)
+        redirect_to dog_path(@dog)
+    end 
 end
