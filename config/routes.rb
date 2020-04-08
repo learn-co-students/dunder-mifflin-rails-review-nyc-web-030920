f979c9184b24f442842d5b55b7cfb8124095a905
Josephscha <Josephshincha@gmail.com>
2020-04-08 00:09:01 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "dogs/popular_index", to: "dogs#popular_index", as: "popular_dogs"
   resources :dogs
   resources :employees
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
