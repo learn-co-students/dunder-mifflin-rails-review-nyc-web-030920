cc0c30effdbde5e5492a4be07c97ef1233ff3445
duruchima <duruchima1@gmail.com>
2020-04-08 19:51:38 -0400

    all deliverables completed, all requested functions operational except for the dog index sort button

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,20 @@
 class DogsController < ApplicationController
+    before_action :find_dog, only: [:show, :update, :edit, :destroy]
+    def index
+        @dogs = Dog.all
+    end
+
+    def show
+    end
+
+
+    private
+
+    def find_dog
+        @dog= Dog.find(params[:id])
+    end
+
+    def dog_params
+        params.require(:dog).permit(:name, :breed, :age)
+    end
 end
